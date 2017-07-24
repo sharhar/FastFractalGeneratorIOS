cbebbb2f37c65c36e3c2fec9351593b06e1f1f7d
Steven Steiner <steets250@gmail.com>
2017-07-24 16:54:17 -0700

    UI Updates
    Added iterations slider and label, and changed the constraint setup for
    other elements.

--- Fast Fractal Generator/MainViewController.swift
+++ Fast Fractal Generator/MainViewController.swift
@@ -13,53 +13,86 @@ import CoreGraphics
 class MainViewController: UIViewController, GLKViewDelegate, GLKViewControllerDelegate  {
 
     var frac: Fractal = Fractal()
+    let context = EAGLContext.init(api: EAGLRenderingAPI.openGLES2)
+    var glView: GLKView!
+    let controller = GLKViewController.init()
+    let button = UIButton.init(type: UIButtonType.roundedRect)
+    let slider = UISlider.init()
+    let label = UILabel.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let context: EAGLContext = EAGLContext.init(api: EAGLRenderingAPI.openGLES2)
-        let glView: GLKView = GLKView.init(frame: self.view.frame)
-        glView.context = context
+        glView = GLKView.init(frame: self.view.frame)
+        glView.context = context!
         glView.delegate = self
 
-        let controller: GLKViewController = GLKViewController.init()
         controller.view = glView
         controller.delegate = self
         controller.preferredFramesPerSecond = 60
 
         self.view.addSubview(glView)
 
-        let button: UIButton = UIButton.init(type: UIButtonType.roundedRect)
         button.setTitle("Change Color", for: UIControlState.normal)
         button.sizeToFit()
         button.layer.cornerRadius = 5
         button.clipsToBounds = true
         button.backgroundColor = UIColor.white
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonIsClicked), for: .touchUpInside)
 
         glView.addSubview(button)
 
-        var constraint: NSLayoutConstraint = NSLayoutConstraint.init(item: button, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: glView, attribute: NSLayoutAttribute.leading, multiplier: 1.0, constant: 30.0)
-        glView.addConstraint(constraint)
+        slider.minimumValue = 0
+        slider.maximumValue = 1000
+        slider.value = 100
+        slider.isContinuous = true
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.addTarget(self, action: #selector(sliderValueDidChange),for: .valueChanged)
 
-        constraint = NSLayoutConstraint.init(item: button, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: glView, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 30.0)
-        glView.addConstraint(constraint)
+        glView.addSubview(slider)
 
-        constraint = NSLayoutConstraint.init(item: button, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.greaterThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 1.0, constant: 20.0)
-        glView.addConstraint(constraint)
-
-        constraint = NSLayoutConstraint.init(item: button, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.greaterThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1.0, constant: 20.0)
-        glView.addConstraint(constraint)
-
-        let label: UILabel = UILabel.init(frame: CGRect(x: 100, y: 300, width: 100, height: 100))
-        label.text = "Hello world"
-        label.textColor = UIColor.white
+        label.text = "Iterations: \(Int(slider.value))"
+        label.textAlignment = .center
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
 
         glView.addSubview(label)
-        
+
+        let sliderHeight = NSLayoutConstraint.init(item: slider, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50)
+        let sliderWidth = NSLayoutConstraint.init(item: slider, attribute: .width, relatedBy: .equal, toItem: glView, attribute: .width, multiplier: 0.75, constant: 0)
+        let sliderX = NSLayoutConstraint.init(item: slider, attribute: .centerX, relatedBy: .equal, toItem: glView, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let sliderY = NSLayoutConstraint.init(item: slider, attribute: .bottom, relatedBy: .equal, toItem: glView, attribute: .bottom, multiplier: 1.0, constant: -32)
+
+        let buttonHeight = NSLayoutConstraint.init(item: button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 32)
+        let buttonWidth = NSLayoutConstraint.init(item: button, attribute: .width, relatedBy: .equal, toItem: glView, attribute: .width, multiplier: 0.5, constant: 0)
+        let buttonX = NSLayoutConstraint.init(item: button, attribute: .centerX, relatedBy: .equal, toItem: glView, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let buttonY = NSLayoutConstraint.init(item: button, attribute: .top, relatedBy: .equal, toItem: glView, attribute: .top, multiplier: 1.0, constant: 32)
+
+        let labelHeight = NSLayoutConstraint.init(item: label, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50)
+        let labelWidth = NSLayoutConstraint.init(item: label, attribute: .width, relatedBy: .equal, toItem: glView, attribute: .width, multiplier: 0.75, constant: 0)
+        let labelX = NSLayoutConstraint.init(item: label, attribute: .centerX, relatedBy: .equal, toItem: glView, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let labelY = NSLayoutConstraint.init(item: label, attribute: .bottom, relatedBy: .equal, toItem: glView, attribute: .bottom, multiplier: 1.0, constant: -4)
+
+        glView.addConstraints([sliderHeight, sliderWidth, sliderX, sliderY, buttonX, buttonY, buttonWidth, buttonHeight, labelHeight, labelWidth, labelX, labelY])
+
         EAGLContext.setCurrent(context)
 
         frac.setup(size: CGPoint(x: self.view.frame.width, y: self.view.frame.height))
+    }
+
+    func sliderValueDidChange(sender: UISlider!) {
+        if Int(sender.value) == 1 {
+            label.text = "Iteration: \(Int(sender.value))"
+        } else {
+            label.text = "Iterations: \(Int(sender.value))"
+        }
+        //Use sender.value to change the fractal generation.
+    }
+
+    func buttonIsClicked(sender: UIButton!) {
+        print("Button Clicked")
+        //React to button being clicked.
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -81,7 +114,6 @@ class MainViewController: UIViewController, GLKViewDelegate, GLKViewControllerDe
     }
 
     func glkView(_: GLKView, drawIn: CGRect) {
-        print("Hello")
         frac.render()
     }
 
